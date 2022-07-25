f972c30607ca03475aa745c64a17719fddcfa9c1
Alla-Anastasiia Gnatkiv <Alla.Gnatkiv@gmail.com>
2022-07-25 16:14:49 -0400

    added function that when user click on item image it goes to a new page where this item is descripted

--- js/nuts.js
+++ js/nuts.js
@@ -518,4 +518,14 @@ clickCheckbox(checkboxAreaDesktop, checkboxesDesktop)
 clickCheckbox(checkboxAreaTablet, checkboxesTablet)
 
 
+//////////////////////////////////////////////////////////////////////////////
+//Onclick on product-item image it goes to a new page where this item is descripted
+
+const imageWrapper = document.querySelectorAll(".image-wrapper")
+for(let i=0; i<imageWrapper.length; i++){
+    imageWrapper[i].addEventListener("click", () => {
+        window.location.href = 'C:/Users/Admin/Desktop/Web/NutsShop/html/product-info.html'
+    })
+}
+
 
