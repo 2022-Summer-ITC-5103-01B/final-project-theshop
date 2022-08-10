35a2684e93951371942ebe1a9a15569da2592306
Alla-Anastasiia Gnatkiv <Alla.Gnatkiv@gmail.com>
2022-08-10 19:32:28 -0400

    fashion section was modified

---
+++ Fashion/assets/javascript/script.js
@@ -0,0 +1,35 @@
+// TOGGLE BUTTON CODE STARTS
+const toggleBtn = document.querySelector(".toggle-btn");
+const mainNavList = document.querySelector(".main-nav-list");
+
+toggleBtn.addEventListener("click", () => {
+  toggleBtn.classList.toggle("active");
+  mainNavList.classList.toggle("active");
+})
+
+document.querySelectorAll(".main-nav-link").forEach(e => e.addEventListener("click", () => {
+  toggleBtn.classList.remove("active");
+  mainNavList.classList.remove("active");
+}))
+
+// Hiding the menu when you click outside it
+document.addEventListener("click", function(e) {
+  if (!toggleBtn.contains(e.target) && !mainNavList.contains(e.target)) {
+    toggleBtn.classList.remove("active");
+    mainNavList.classList.remove("active");
+  }
+})
+// TOGGLE BUTTON CODE ENDS
+
+
+
+var MenuItems = document.getElementById("MenuItems");
+MenuItems.style.maxHeight = "0px";
+function menutoggle() {
+    if (MenuItems.style.maxHeight == "20px") {
+        MenuItems.style.maxHeight = "200px";
+    }
+    else {
+        MenuItems.style.maxHeight = "0px";
+    }
+}
