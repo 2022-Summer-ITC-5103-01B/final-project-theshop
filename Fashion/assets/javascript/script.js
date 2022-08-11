a94c8d65eb0d96c8d8c8809569ea50950528ffb0
Alla-Anastasiia Gnatkiv <Alla.Gnatkiv@gmail.com>
2022-08-11 02:34:30 -0400

    Link changes in header and footer

--- Fashion/assets/javascript/script.js
+++ Fashion/assets/javascript/script.js
@@ -1,3 +1,35 @@
+let zoomer = function (){
+  document.querySelector('#img-zoomer-box')
+    .addEventListener('mousemove', function(e) {
+
+    let original = document.querySelector('#img-1'),
+        magnified = document.querySelector('#img-2'),
+        style = magnified.style,
+        x = e.pageX - this.offsetLeft,
+        y = e.pageY - this.offsetTop,
+        imgWidth = original.offsetWidth,
+        imgHeight = original.offsetHeight,
+        xperc = ((x/imgWidth) * 100),
+        yperc = ((y/imgHeight) * 100);
+
+    //lets user scroll past right edge of image
+    if(x > (.01 * imgWidth)) {
+      xperc += (.15 * xperc);
+    };
+
+    //lets user scroll past bottom edge of image
+    if(y >= (.01 * imgHeight)) {
+      yperc += (.15 * yperc);
+    };
+
+    style.backgroundPositionX = (xperc - 9) + '%';
+    style.backgroundPositionY = (yperc - 9) + '%';
+
+    style.left = (x - 180) + 'px';
+    style.top = (y - 180) + 'px';
+
+  }, false);
+}();
 // TOGGLE BUTTON CODE STARTS
 const toggleBtn = document.querySelector(".toggle-btn");
 const mainNavList = document.querySelector(".main-nav-list");
@@ -21,15 +53,36 @@ document.addEventListener("click", function(e) {
 })
 // TOGGLE BUTTON CODE ENDS
 
+// 'hero-img' SLIDESHOW CODE STARTS
+// let slideIndex = 0;
+// carousel();
 
+// function carousel() {
+//   let i;
+//   let x = document.getElementsByClassName("slides");
 
-var MenuItems = document.getElementById("MenuItems");
-MenuItems.style.maxHeight = "0px";
-function menutoggle() {
-    if (MenuItems.style.maxHeight == "20px") {
-        MenuItems.style.maxHeight = "200px";
-    }
-    else {
-        MenuItems.style.maxHeight = "0px";
-    }
-}
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";
+//   }
+
+//   slideIndex++;
+
+//   if (slideIndex > x.length) {
+//     slideIndex = 1;
+//   }
+
+//   x[slideIndex - 1].style.display = "block";
+//   setTimeout(carousel, 3000);
+// }
+// // 'hero-img' SLIDESHOW CODE ENDS
+
+// var MenuItems = document.getElementById("MenuItems");
+// MenuItems.style.maxHeight = "0px";
+// function menutoggle() {
+//     if (MenuItems.style.maxHeight == "20px") {
+//         MenuItems.style.maxHeight = "200px";
+//     }
+//     else {
+//         MenuItems.style.maxHeight = "0px";
+//     }
+// }
